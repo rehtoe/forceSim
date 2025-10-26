2c8fcbd2c65938f6921e21553b12484650de8fef
Ernesto <ejp1077@gmail.com>
2025-10-25 23:33:20 -0700

    pre-fix forces for save/reference

--- source/physics/forces.cpp
+++ source/physics/forces.cpp
@@ -1,1 +1,57 @@
 ///     forces.cpp
+
+///     Force 
+void Force::update_direction(){
+    chx_con = false;
+    if(tail && arrow){
+        dx = arrow->get_position().x - tail->get_position().x;
+        dy = arrow->get_position().y - tail->get_position().y;
+        dz = arrow->get_position().z - tail->get_position().z;
+        direction = Vector3d(dx,dy,dz);
+    } 
+}
+void Force::calculate_force(double timestep_pass){
+    update_direction();
+}
+void Force::update_objects(double timestep){
+    /* acceleratio calculation etc */
+}
+const double& Force::get_force_magnitude() const{
+    if(!chx_con){ calculate_force(); }
+    return force_magnitude;
+}
+const Vector<DIMENSIONS>& Force::get_force_vector() const { 
+    if(!chx_con){ calculate_force(); }
+    return force_vector; 
+}
+const Vector<DIMENSIONS>& Force::get_direction() const{ return direction; }
+const PhysicsObject& Force::get_start_object() const {
+    if(!tail){ throw std::runtime_error("No start object set"); }
+    return *tail;
+}
+const PhysicsObject& Force::get_end_object() const { 
+    if(!arrow){ throw std::runtime_error("No end object set");
+    return *arrow;
+}
+void Force::change_start_object(PhysicsObject* new_object){ 
+    chx_con = false; 
+    tail = new_object;
+    update_direction();
+}
+void Force::change_end_object(PhysicsObject* new_object){
+    chx_con = false; 
+    arrow = new_object; 
+    update_direction();
+}
+void Force::change_objects(PhysicsObject* new_tail, PhysicsObject* new_arrow){ 
+    chx_con = false; 
+    tail = new_tail; 
+    arrow = new_arrow; 
+    update_direction(); 
+}
+void Force::step(double dt){
+    if(dt<0) { return; }
+    calculate_force();
+    update_objects();
+    chx_con = true;
+}
--- source/physics/forces.h
+++ source/physics/forces.h
@@ -1,7 +1,10 @@
 ///     forces.h
 #include "../maths/vector3d.h"
+#include "objects.h"
 #include "../data/atoms.h"
 #include "../data/particles.h"
+
+#include <iostream>
 
 /*
   FORCES:           RANGE:          COMMON PARTICLE AFFECTED
@@ -17,50 +20,58 @@ enum class ForceType{
     Strong_Nuclear
 };
 
+
 class Force{
   private: //       PRIVATE
     //  PROPERTIES
-    Vector3d direction; //  position vector from point force is exerted on
-    Particle tail; //   first particle, tail end of vector
-    Particle arrow; //  second particle, arrow end of vector
+    Vector direction; //  position vector from point force is exerted on
+    PhysicsObject* tail; //   first PhysicsObject, tail end of vector
+    PhysicsObject* arrow; //  second PhysicsObject, arrow end of vector
+    //    mutable allows it to be changing variable whilst being used by const functions
+    mutable bool chx_con; //    changed conditions, ie force has been exerted/stepped, change in objects
+    mutable double force_magnitude;   //    magnitude of force, cache, used for const reference getter
+    mutable Vector3d force_vector;
     //  METHODS
-    virtual void update_particles();
+    void update_direction(); // chx_con false, when changing objects or stepping/exerting forces
+    //  also has no base class or derivation as all forces should have a type or inherited scopes
+    virtual void calculate_force() = 0; //  calculates the force_vec and mag
+    virtual void update_objects(); // chx_con false, uses force_vec to move objects
   public: //        PUBLLIC
     //  CONSTRUCTORS
-    Force():direction(Vector3d(0.0,0.0,0.0)),tail(Particle()),arrow(Particle()){};
+    Force():direction(Vector3d(0.0,0.0,0.0)),tail(nullptr),arrow(nullptr){};
     //  METHODS
-    virtual double magnitude() const;
-    Vector3d& get_direction();
-    Particle& get_start_particle();
-    Particle& get_end_particle();
-    void update_direction();
-    void change_start_particle(Particle new_particle);
-    void change_end_particle(Particle new_particle);
-    void step(double dt);
+    const double& get_force_magnitude() const;
+    const Vector3d& get_force_vector() const;
+    const Vector3d& get_direction() const;
+    const PhysicsObject& get_start_object() const;
+    const PhysicsObject& get_end_object() const;
+    void change_start_object(PhysicsObject* new_object); // chx_con false
+    void change_end_object(PhysicsObject* new_object); // chx_con false
+    void change_objects(PhysicsObject* new_tail, PhysicsObject* new_arrow);
+    void step(double dt); // chx_con false
 
 };
 
-class GravitationalForce:Force{
+class GravitationalForce: public Force{
     //  PROPERTIES
     //  CONSTRUCTORS
     GravitationalForce();
     //  METHODS
 };
-class ElectricForce:Force{
+class ElectricForce: public Force{
     //  PROPERTIES
     //  CONSTRUCTORS
     ElectricForce();
     //  METHODS
-    double magnitude() const override;
 };
-class WeakNuclearForce:Force{
+class WeakNuclearForce: public Force{
     //  PROPERTIES
 
     //  CONSTRUCTORS
     WeakNuclearForce();
     //  METHODS
 };
-class StrongNuclearForce:Force{
+class StrongNuclearForce: public Force{
     //  PROPERTIES
     //  CONSTRUCTORS
     StrongNuclearForce();
